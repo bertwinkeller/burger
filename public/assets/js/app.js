c990da54e6e8166b47cdcae183b7b9aa5ae6da31
Bert Keller <bertwinkeller@gmail.com>
2019-12-01 19:11:41 -0800

    front end js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,41 @@
+function getBurgers(){
+    axios.get('/burgers')
+    .then(burgers => {
+        console.log(burgers)
+    })
+    .catch(e => console.log(e))
+}
+
+function addBurger(burger){
+    axios.post('/burgers', {
+        name: burger,
+        eaten: false
+    })
+    .then(() => {
+        console.log('Burger added')
+    })
+    .catch(e => console.log(e))
+}
+
+function eatBurger(){
+    
+}
+function removeBurger(){
+
+}
+
+
+
+
 document.getElementById('addBurger')
 .addEventListener('click', e => {
     e.preventDefault()
     console.log('add burger')
+    addBurger(document.getElementById('burger').value)
 }
 )
+
+document.addEventListener('click',  event => {
+    if(event.target.className.includes('eatBurger')){
+    }else if (event.target.className.includes('deleteBurger'))
+})
