03afcd25b2598bbf153e1cbeb715061bf03727b4
Bert Keller <bertwinkeller@gmail.com>
2019-12-03 11:43:30 -0800

    new config info

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 
 module.exports = require('mysql2').createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'burger_db'
+    host: 'hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'fpy65vj3481cwa0b',
+    password: 'kuast5c2g3c675fg',
+    database: 'bwq9eadovbx5c5wo'
 });
